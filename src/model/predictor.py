18c23568f232051a3c76c6314a4894695c405150
Tiburso <manuelmariamc@gmail.com>
2024-03-01 19:24:05 +0100

    Fix input output mismatch

--- src/model/predictor.py
+++ src/model/predictor.py
@@ -13,11 +13,8 @@ class LSTMModel(torch.nn.Module):
         self.fc = torch.nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        out, _ = self.lstm(x)
 
-        out, _ = self.lstm(x, (h0, c0))
-
-        out = self.fc(out[:, -1, :])
+        out = self.fc(out)
 
         return out
