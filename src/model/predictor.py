3c0051670ab4207425f70eb737e6f85f46e4ec9e
Tiburso <manuelmariamc@gmail.com>
2024-03-07 18:06:12 +0100

    Refactor LSTMModel to use pytorch_lightning

--- src/model/predictor.py
+++ src/model/predictor.py
@@ -1,7 +1,8 @@
 import torch
+import pytorch_lightning as pl
 
 
-class LSTMModel(torch.nn.Module):
+class LSTMModel(pl.LightningModule):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(LSTMModel, self).__init__()
 
@@ -14,7 +15,21 @@ class LSTMModel(torch.nn.Module):
 
     def forward(self, x):
         out, _ = self.lstm(x)
-
         out = self.fc(out)
-
         return out
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = torch.nn.functional.mse_loss(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = torch.nn.functional.mse_loss(y_hat, y)
+        self.log("val_loss", loss)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
